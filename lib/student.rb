3fb0b7ac47efc96a61dde9344aef7733b4923c96
Corban <sonnemanc@gmail.com>
2021-09-26 13:31:15 -0600

    Done with Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,28 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
+
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student = self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    #This metaprogramming is to add the social media/blog links
+    attributes_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
